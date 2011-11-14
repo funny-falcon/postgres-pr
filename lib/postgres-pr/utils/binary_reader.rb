a8bc6926162031a30d043d18a8c0135841e3d050
Sokolov Yura <funny.falcon@gmail.com>
2011-11-14 19:53:21 +0400

    cleanup bunary_reader

--- lib/postgres-pr/utils/binary_reader.rb
+++ lib/postgres-pr/utils/binary_reader.rb
@@ -21,41 +21,7 @@ module PostgresPR
       alias read_byte read_word8
       
       # == 16 bit
-
-      # === Unsigned
-      
-      if ByteOrder.little_endian?
-        def read_word16_native
-          read_word16_little
-        end
-      else
-        def read_word16_native
-          read_word16_big
-        end
-      end
-
-      def read_word16_little
-        fst = readbyte
-        scd = readbyte
-        scd * 256 + fst
-      end
-
-      def read_word16_big
-        fst = readbyte
-        scd = readbyte
-        fst * 256 + scd
-      end
-
       # === Signed
-
-      def read_int16_native
-        ru(2, 's')
-      end
-
-      def read_int16_little
-        # swap bytes if native=big (but we want little)
-        ru_swap(2, 's', ByteOrder::Big)
-      end
 
       def read_int16_big
         # swap bytes if native=little (but we want big)
@@ -63,31 +29,7 @@ module PostgresPR
       end
 
       # == 32 bit
-
-      # === Unsigned
-
-      def read_word32_native
-        ru(4, 'L')
-      end
-
-      def read_word32_little
-        ru(4, 'V')
-      end
-
-      def read_word32_big
-        (((read_word8 * 256) + read_word8) * 256 + read_word8) * 256 + read_word8
-      end
-
       # === Signed
-
-      def read_int32_native
-        ru(4, 'l')
-      end
-
-      def read_int32_little
-        # swap bytes if native=big (but we want little)
-        ru_swap(4, 'l', ByteOrder::Big) 
-      end
 
       def read_int32_big
         (((read_int8 * 256) + read_word8) * 256 + read_word8) * 256 + read_word8
@@ -96,38 +38,8 @@ module PostgresPR
       # == Aliases
 
       alias read_uint8 read_word8
-
-      # add some short-cut functions 
-      %w(word16 int16 word32 int32).each do |typ|
-        alias_method "read_#{typ}_network", "read_#{typ}_big"
-      end
-
-      {:word16 => :uint16, :word32 => :uint32}.each do |old, new|
-        ['_native', '_little', '_big', '_network'].each do |bo|
-          alias_method "read_#{new}#{bo}", "read_#{old}#{bo}"
-        end
-      end
-
-      # read exactly n characters, otherwise raise an exception.
-      def readn(n)
-        str = read(n)
-        raise "couldn't read #{n} characters" if str.nil? or str.size != n
-        str
-      end
-
-      private
-
-      # shortcut method for readn+unpack
-      def ru(size, template)
-        readn(size).unpack(template).first
-      end
-
-      # same as method +ru+, but swap bytes if native byteorder == _byteorder_  
-      def ru_swap(size, template, byteorder)
-        str = readn(size)
-        str.reverse! if ByteOrder.byteorder == byteorder 
-        str.unpack(template).first
-      end
+      alias read_int16_network read_int16_big
+      alias read_int32_network read_int32_big
     end
   end
 end
