20ceb5b11329c964f78be5d8b30ab7d446270996
Sokolov Yura <funny.falcon@gmail.com>
2011-11-18 14:10:03 +0400

    allow to test engines separetely

--- test_speed/test_speed.sh
+++ test_speed/test_speed.sh
@@ -1,4 +1,6 @@
 for vm in 1.9.3 ree rbx jruby ; do
-  echo $vm
-  rvm $vm exec ruby test_speed.rb | tee new_${vm}.txt
+  if [ -z "$1" -o "$1" = $vm ] ; then
+    echo $vm
+    rvm $vm exec ruby test_speed.rb | tee new_${vm}.txt
+  fi
 done
