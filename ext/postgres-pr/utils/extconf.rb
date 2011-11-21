158d6bd1cad9f0de92fa66d22f9bb3d15ce233e8
Sokolov Yura <funny.falcon@gmail.com>
2011-11-21 12:22:04 +0400

    skip building on not Matz Ruby

--- ext/postgres-pr/utils/extconf.rb
+++ ext/postgres-pr/utils/extconf.rb
@@ -1,3 +1,8 @@
-require 'mkmf'
-
-create_makefile("unpack_single")
+if RUBY_ENGINE == 'ruby'
+  require 'mkmf'
+  create_makefile("unpack_single")
+else
+  File.open(File.dirname(__FILE__) + "/Makefile", 'w') do |f|
+    f.write("install:\n\t#nothing to build")
+  end
+end
